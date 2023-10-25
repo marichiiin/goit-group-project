b57d71965c933d00889d64b41918c50346458101
Mari <40849185+marichiiin@users.noreply.github.com>
2023-10-25 13:14:24 +0800

    normal

---
+++ index.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "4",
+  freeMode: true,
+  //spaceBetween: 18,
+  grabCursor: true,
+    
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+ 
+    });
